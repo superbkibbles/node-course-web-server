47219bce0a9ef23a4977922661edfc0d7308bb09
Mand <mend_mzury@hotmail.com>
2018-11-14 21:17:34 +0200

    here is done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const hbs = require("hbs")
 const fs = require("fs")
 var app = express()
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "hbs")
 hbs.registerPartials(__dirname + "/views/partials")
@@ -57,6 +58,6 @@ app.get("/bad", (req, res)=>{
   })
 })
 
-app.listen(3000, function(){
-  console.log("connected")
+app.listen(port, function(){
+  console.log(  `connected ${port}`)
 })
